02e9b5217a6e7084ef90dd26f0e66f36177f44b6
zxhuan <1123046380@qq.com>
2020-01-12 22:23:10 +0800

    test

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,113 +1,105 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li>
-        <a
-          href="https://vuejs.org"
-          target="_blank"
-        >
-          Core Docs
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://forum.vuejs.org"
-          target="_blank"
-        >
-          Forum
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://chat.vuejs.org"
-          target="_blank"
-        >
-          Community Chat
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://twitter.com/vuejs"
-          target="_blank"
-        >
-          Twitter
-        </a>
-      </li>
-      <br>
-      <li>
-        <a
-          href="http://vuejs-templates.github.io/webpack/"
-          target="_blank"
-        >
-          Docs for This Template
-        </a>
-      </li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li>
-        <a
-          href="http://router.vuejs.org/"
-          target="_blank"
-        >
-          vue-router
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vuex.vuejs.org/"
-          target="_blank"
-        >
-          vuex
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vue-loader.vuejs.org/"
-          target="_blank"
-        >
-          vue-loader
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-        >
-          awesome-vue
-        </a>
-      </li>
-    </ul>
+  <div class="box">
+    <div class="box1">
+      <ul class="ul-list">
+        <li class="list-item" v-for="(item,key) of dataList" :key='key' draggable="true"
+          @dragstart="onDragstart($event,item)">
+          {{item.name}}111</li>
+      </ul>
+      <div class="box2" @dragend="onDragend($event)" @dragover="onDragover($event)" @drop="onDrop($event)"
+        data-listName='list1'>
+        <div class="box-item" v-for="(item,key) of list1">
+          <span>{{item.name}}</span><span>{{item.name}}</span>
+        </div>
+      </div>
+      <div class="box3" @dragend="onDragend($event)" @dragover="onDragover($event)" @drop="onDrop($event)"
+        data-listName='list2'>
+        <div class="box-item" v-for="(item,key) of list2">
+          <span>{{item.name}}</span><span>{{item.name}}</span>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
-
 <script>
-export default {
-  name: 'HelloWorld',
-  data () {
-    return {
-      msg: 'Welcome to Your Vue.js App,my name is zxhuan,hello world!!!'
+  export default {
+    data() {
+      return {
+        dataList: [
+          { id: 1, name: 11 },
+          { id: 2, name: 12 },
+          { id: 3, name: 13 },
+        ],
+        activeItem: {},
+        list1: [],
+        list2: [],
+      }
+    },
+    methods: {
+      onDragstart($event, item) {
+        this.activeItem = item;
+        console.log('dargestart')
+      },
+      getParentNode(target, dataName) {
+        let _name = target.getAttribute(`data-${dataName}`);
+        if (!_name) {
+          return this.getParentNode(target.parentNode, dataName)
+        }
+        return _name;
+      },
+      onDrop($event) {
+        let listName = this.getParentNode($event.target, 'listName')
+        let _index = this[listName].findIndex(item => item.id == this.activeItem.id);
+        if (_index >= 0) {
+          console.log('该数据已经存在')
+        } else {
+          this[listName].push(this.activeItem);
+        }
+        console.log('drop')
+
+      },
+      onDragover($event) {
+        console.log('dragover')
+        $event.preventDefault()
+      },
+      onDragend($event) {
+        console.log('dragend')
+      },
     }
   }
-}
 </script>
+<style>
+  .box {
+    height: 1000px;
+    width: 100%;
+  }
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h1, h2 {
-  font-weight: normal;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+  .box1 {
+    list-style-type: none;
+    margin: 0;
+  }
+
+  li {
+    height: 35px;
+    list-style-type: none;
+    margin: 0;
+    cursor: move;
+
+  }
+
+  li:hover {
+    background: yellow;
+  }
+
+  .box2 {
+    height: 300px;
+    width: 300px;
+    background: red;
+  }
+
+  .box3 {
+    height: 300px;
+    width: 300px;
+    background: green;
+  }
 </style>
